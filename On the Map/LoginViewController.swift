631a1f644f8d44cc949e986c521da5a9977c127e
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-01-21 18:07:29 +0300

    Add IBOutelts for Login view

--- On the Map/LoginViewController.swift
+++ On the Map/LoginViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
