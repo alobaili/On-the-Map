39a665912ee5198e67f0fe5d2da6d7bbbd6da5cc
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-01-21 18:30:16 +0300

    Add Constants file with Parse base URL and parameter keys

---
+++ On the Map/Constants.swift
@@ -0,0 +1,32 @@
+//
+//  Constants.swift
+//  On the Map
+//
+//  Created by Abdulaziz AlObaili on 21/01/2019.
+//  Copyright © 2019 Abdulaziz Alobaili. All rights reserved.
+//
+
+// MARK: - Constants
+struct Constants {
+    
+    
+    // MARK: Parse
+    struct Parse {
+        static let APIBaseURL = "https://parse.udacity.com/parse/classes/"
+    }
+    
+    // MARK: Parse Parameter Keys
+    struct ParseParameterKeys {
+        static let ObjectID = "objectId"
+        static let UniqueKey = "uniqueKey"
+        static let FirstName = "firstName"
+        static let LastNAme = "lastName"
+        static let MapString = "mapString"
+        static let MediaURL = "mediaURL"
+        static let Latitude = "latitude"
+        static let Longitude = "longitude"
+        static let CreatedAt = "createdAt"
+        static let UpdatedAt = "updatedAt"
+        static let ACL = "ACL"
+    }
+}
