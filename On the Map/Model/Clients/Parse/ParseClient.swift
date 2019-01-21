0e70c4343512f083ecd881c64024cfc7752b0835
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-01-21 19:12:03 +0300

    Add client and convenience files

---
+++ On the Map/Model/Clients/Parse/ParseClient.swift
@@ -0,0 +1,9 @@
+//
+//  ParseClient.swift
+//  On the Map
+//
+//  Created by Abdulaziz AlObaili on 21/01/2019.
+//  Copyright © 2019 Abdulaziz Alobaili. All rights reserved.
+//
+
+import Foundation
---
+++ On the Map/Model/Clients/Udacity/UdacityClient.swift
@@ -0,0 +1,9 @@
+//
+//  UdacityClient.swift
+//  On the Map
+//
+//  Created by Abdulaziz AlObaili on 21/01/2019.
+//  Copyright © 2019 Abdulaziz Alobaili. All rights reserved.
+//
+
+import Foundation
