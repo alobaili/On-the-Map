6372506286abdba96404fb64acdfd20a62d82df1
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-01-23 20:46:28 +0300

    Create StudentLocation struct

---
+++ On the Map/Model/StudentLocation.swift
@@ -0,0 +1,24 @@
+//
+//  StudentLocation.swift
+//  On the Map
+//
+//  Created by Abdulaziz AlObaili on 23/01/2019.
+//  Copyright © 2019 Abdulaziz Alobaili. All rights reserved.
+//
+
+import Foundation
+
+struct StudentLocation : Codable {
+    
+    let createdAt : String?
+    let firstName : String?
+    let lastName : String?
+    let latitude : Double?
+    let longitude : Double?
+    let mapString : String?
+    let mediaURL : String?
+    let objectId : String?
+    let uniqueKey : String?
+    let updatedAt : String?
+    
+}
