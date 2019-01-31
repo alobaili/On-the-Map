e002df1766bcc7addc8c22f67dfc3e5583d3869c
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-02-01 01:37:29 +0300

    Feature: Add Location

--- On the Map/Controller/AddLocationViewController.swift
+++ On the Map/Controller/AddLocationViewController.swift
@@ -8,11 +8,15 @@
 
 import Foundation
 import UIKit
+import CoreLocation
 
 class AddLocationViewController: UIViewController {
     
-    @IBOutlet weak var locationTectField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
     @IBOutlet weak var mediaURLTextField: UITextField!
+    
+    var longitude: Double?
+    var latitude: Double?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +28,42 @@ class AddLocationViewController: UIViewController {
     
     
     @IBAction func findLocationPressed(_ sender: Any) {
+        guard let location = locationTextField.text,
+        let mediaURL = mediaURLTextField.text,
+        location != "",
+        mediaURL != "" else {
+            let alert = UIAlertController(title: "Required field", message: "Please fill both the location and URL", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+                return
+            }))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        let studentLocation = StudentLocation.init(createdAt: "", firstName: nil, lastName: nil, latitude: 0, longitude: 0, mapString: location, mediaURL: mediaURL, objectId: "", uniqueKey: "", updatedAt: "")
+        
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(studentLocation.mapString!) { (placeMarks, _) in
+            guard let marks = placeMarks else {
+                let alert = UIAlertController(title: "Error", message: "Could't find your location, please try again.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+                    return
+                }))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            
+            var studentLocation = studentLocation
+            studentLocation.longitude = Double(((marks.first!.location?.coordinate.longitude)!))
+            studentLocation.latitude = Double(((marks.first!.location?.coordinate.latitude)!))
+            
+            self.performSegue(withIdentifier: "toConfirmLocation", sender: studentLocation)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toConfirmLocation" {
+            let viewController = segue.destination as! ConfirmLocationViewController
+            viewController.studentLocation = (sender as! StudentLocation)
+        }
     }
 }
