ffc1f9badbf88532463ee11002ee4d12f5d16ea2
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-01-31 03:24:48 +0300

    Add view from adding new location

---
+++ On the Map/Controller/AddLocationViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddLocationViewController.swift
+//  On the Map
+//
+//  Created by Abdulaziz AlObaili on 31/01/2019.
+//  Copyright © 2019 Abdulaziz Alobaili. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddLocationViewController: UIViewController {
+    
+}
