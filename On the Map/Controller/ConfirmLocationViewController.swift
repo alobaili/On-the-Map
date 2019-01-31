4ce7278a593fe2d33586611267511662f70fc85b
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-02-01 01:38:19 +0300

    Add Confirm Location view with starter code

---
+++ On the Map/Controller/ConfirmLocationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ConfirmLocationViewController.swift
+//  On the Map
+//
+//  Created by Abdulaziz AlObaili on 01/02/2019.
+//  Copyright © 2019 Abdulaziz Alobaili. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmLocationViewController: UIViewController {
+    
+    
+    var studentLocation: StudentLocation?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
